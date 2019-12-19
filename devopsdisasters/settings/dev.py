bd5fc4bb0cb50bad4d816113e96d4ff9e4768634
Stefan Walluhn <stefan.walluhn@aboutsource.net>
2019-12-19 10:50:16 +0100

    Initital commit, wagtail scaffold

---
+++ devopsdisasters/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'g^*(8f=mi^&cde&a(s3zov$)^i*t+z_!2(2ii_jga*926sqx$l'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
